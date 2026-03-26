a48fefe3d8d0ea105bcd1b4b056e8a2cedaa4122
= <=>
2026-03-27 01:08:14 +0530

    create Folder structure For Project

---
+++ template.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Createing Directory
+mkdir -p src
+mkdir -p research
+
+# Creating Files
+touch src/__init__.py
+touch src/helper.py
+touch research/prompt.py
+touch .env
+touch setUp.py
+touch app.py
+touch research/trial.ipynb
+touch requirements.txt
+
+
+echo "Directory And FIle Created Successfully"
