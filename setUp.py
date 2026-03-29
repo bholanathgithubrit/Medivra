5bd9c641ccc0b21a748b25b197825c7cfb7a9621
= <=>
2026-03-30 02:30:30 +0530

    create vector embedding and rag pipeline and generate content based on user need

---
+++ setUp.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Bholanath Maity",
+    author_email="maitybholanath56@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
