6061eea033efa4f5440f2ba0c0bb76593c425eca
= <=>
2026-04-07 03:28:00 +0530

    change default port

--- src/helper.py
+++ src/helper.py
@@ -20,7 +20,7 @@ def load_pdf_file(data):
 def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
     """
     Given a list of Document objects, return a new list of Document objects
-    containing only 'source' in metadata and the original page_content.
+    containing only 'source' in metadata and the original page_content. 
     """
     minimal_docs: List[Document] = []
     for doc in docs:
